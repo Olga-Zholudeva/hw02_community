bb3697da0cddbd28a6bbc221398c00eb53c47bd5
Ольга Жолудева <joludeva.olga@yandex.ru>
2022-08-26 11:25:22 +0300

    Устранены замечания первого код-ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,25 +5,40 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы'
+        )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Слаг')
+    description = models.TextField(
+        verbose_name='Описание'
+    )
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        verbose_name='Содержание'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата написания'
+        )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Тематическая группа'
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,21 @@
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post, Group
 
 
 def index(request):
-
-    posts = Post.objects.order_by('-pub_date')[:10]
-    context = {'posts': posts, }
+    posts = Post.objects.order_by('-pub_date')[:settings.LIMING_OF_POSTS]
+    context = {
+        'posts': posts,
+    }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    context = {'group': group, 'posts': posts, }
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:settings.LIMING_OF_POSTS]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
     return render(request, 'posts/group_list.html', context)
