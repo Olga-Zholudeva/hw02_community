de4724e4f5d0feee18f1cdd0236df5bfa1e46474
Ольга Жолудева <joludeva.olga@yandex.ru>
2022-08-26 11:50:51 +0300

    Устранены замечания при загрузке проекта: 2 строки перед методом

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 LIMIT_POSTS = 10
+
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:LIMIT_POSTS]
     context = {
